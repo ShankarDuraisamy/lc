74dc3838016d9c4303ad69acd70bb5806f4ef6d1
Duraisamy, Shankar <Shankar.Duraisamy@Teradata.com>
2024-01-01 19:15:28 +0530

    Problem 88

--- py/src/easy/problem_set_1.py
+++ py/src/easy/problem_set_1.py
@@ -3,14 +3,37 @@ from typing import List
 
 class ProblemSet1:
 
-	@staticmethod
-	def assign_cookies_455(g: List[int], s: List[int]) -> int:
-		g.sort()
-		s.sort()
-		pointer_1 = 0
-		pointer_2 = 0
-		while pointer_1 < len(g) and pointer_2 < len(s):
-			if g[pointer_1] <= s[pointer_2]:
-				pointer_1 += 1
-			pointer_2 += 1
-		return pointer_1
+    @staticmethod
+    def assign_cookies_455(g: List[int], s: List[int]) -> int:
+        g.sort()
+        s.sort()
+        pointer_1 = 0
+        pointer_2 = 0
+        while pointer_1 < len(g) and pointer_2 < len(s):
+            if g[pointer_1] <= s[pointer_2]:
+                pointer_1 += 1
+            pointer_2 += 1
+        return pointer_1
+
+    @staticmethod
+    def merge_sorted_array_88(nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        num1_pointer = m - 1
+        num2_pointer = n - 1
+        last_pointer = m + n - 1
+        while num1_pointer >= 0 and num2_pointer >= 0:
+            if nums1[num1_pointer] >= nums2[num2_pointer]:
+                nums1[last_pointer] = nums1[num1_pointer]
+                num1_pointer -= 1
+            else:
+                nums1[last_pointer] = nums2[num2_pointer]
+                num2_pointer -= 1
+            last_pointer -= 1
+        while num2_pointer >= 0:
+            nums1[last_pointer] = nums2[num2_pointer]
+            last_pointer -= 1
+            num2_pointer -= 1
+
+
+
+
+
--- py/test/easy/test_problem_sert_1.py
+++ py/test/easy/test_problem_sert_1.py
@@ -12,3 +12,33 @@ def test_assign_cookies_455_base_case_01():
 	g = [1, 2]
 	s = [1, 2, 3]
 	assert 2 == ProblemSet1.assign_cookies_455(g, s)
+
+
+def test_merge_sorted_array_88_case_01():
+	nums1 = [1, 2, 3, 0, 0, 0]
+	m = 3
+	nums2 = [2, 5, 6]
+	n = 3
+	ProblemSet1.merge_sorted_array_88(nums1, m, nums2, n)
+	assert nums1 == [1, 2, 2, 3, 5, 6]
+	assert len(nums1) == m + n
+
+
+def test_merge_sorted_array_88_case_02():
+	nums1 = [1, 2, 3, 0, 0, 0]
+	m = 3
+	nums2 = [4, 5, 6]
+	n = 3
+	ProblemSet1.merge_sorted_array_88(nums1, m, nums2, n)
+	assert nums1 == [1, 2, 3, 4, 5, 6]
+	assert len(nums1) == m + n
+
+
+def test_merge_sorted_array_88_case_03():
+	nums1 = [0]
+	m = 0
+	nums2 = [1]
+	n = 1
+	ProblemSet1.merge_sorted_array_88(nums1, m, nums2, n)
+	assert nums1 == [1]
+	assert len(nums1) == m + n
